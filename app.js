9edcdc777243df2c17d921a0aa907a0b1f53008f
Oleg Zharov <levineye@gmail.com>
2021-02-09 12:26:46 +0300

    feat: добавить mongoose и подключиться к mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const { SERVER_PORT } = require('./config');
+const { SERVER_PORT, DB_IP, DB_PORT, DB_NAME } = require('./config');
 
 const app = express();
+
+mongoose.connect(`mongodb://${DB_IP}:${DB_PORT}/${DB_NAME}`, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.listen(SERVER_PORT);
--- config.js
+++ config.js
@@ -4,6 +4,9 @@ const {
   NODE_ENV = 'development',
   JWT_SECRET = 'JWT_SECRET_KEY',
   SERVER_PORT = 3000,
+  DB_IP = '127.0.0.1',
+  DB_PORT = '27017',
+  DB_NAME = 'moviesExplorer',
 } = process.env;
 
-module.exports = { NODE_ENV, JWT_SECRET, SERVER_PORT };
+module.exports = { NODE_ENV, JWT_SECRET, SERVER_PORT, DB_IP, DB_PORT, DB_NAME };
