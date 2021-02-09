00bbe2dd423e387466b7f9ba0cbffba18ab3f4b0
Oleg Zharov <levineye@gmail.com>
2021-02-09 13:09:09 +0300

    feat: создать модель юзера Для валидации используется пакет validator

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, 'Поле "email" должно быть заполнено'],
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Поле "email" должно быть валидным url-адресом',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле "password" должно быть заполнено'],
+    select: false,
+  },
+  name: {
+    type: String,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minLength: [2, 'Минимальная длина поля "name" - 2'],
+    maxLength: [30, 'Максимальная длина поля "name" - 30'],
+  },
+});
+
+module.exports = model('user', userSchema);
