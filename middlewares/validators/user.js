082fe0fad1708fa13facbfd74b54cd38d909a175
Oleg Zharov <levineye@gmail.com>
2021-02-12 21:48:59 +0300

    feat: fix: добавить валидацию контроллеров юзера

---
+++ middlewares/validators/user.js
@@ -0,0 +1,59 @@
+const { celebrate, Joi } = require('celebrate');
+
+const validateRegistration = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string()
+      .required()
+      .email()
+      .message('Поле "email" должно быть валидным email-адресом')
+      .messages({
+        'any.required': 'Поле "email" должно быть заполнено',
+      }),
+    password: Joi.string().required().messages({
+      'any.required': 'Поле "password" должно быть заполнено',
+    }),
+    name: Joi.string().required().min(2).max(30).messages({
+      'any.required': 'Поле "name" должно быть заполнено',
+      'string.min': 'Минимальная длина поля "name" - 2',
+      'string.max': 'Максимальная длина поля "name" - 30',
+    }),
+  }),
+});
+
+const validateAuthorization = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string()
+      .required()
+      .email()
+      .message('Поле "email" должно быть валидным email-адресом')
+      .messages({
+        'any.required': 'Поле "email" должно быть заполнено',
+      }),
+    password: Joi.string().required().messages({
+      'any.required': 'Поле "password" должно быть заполнено',
+    }),
+  }),
+});
+
+const validateUserUpdate = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string()
+      .required()
+      .email()
+      .message('Поле "email" должно быть валидным email-адресом')
+      .messages({
+        'any.required': 'Поле "email" должно быть заполнено',
+      }),
+    name: Joi.string().required().min(2).max(30).messages({
+      'any.required': 'Поле "name" должно быть заполнено',
+      'string.min': 'Минимальная длина поля "name" - 2',
+      'string.max': 'Максимальная длина поля "name" - 30',
+    }),
+  }),
+});
+
+module.exports = {
+  validateRegistration,
+  validateAuthorization,
+  validateUserUpdate,
+};
