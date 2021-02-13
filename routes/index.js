489b1921fbf8a2109c420885adf9eb78757da8d0
Oleg Zharov <levineye@gmail.com>
2021-02-13 18:19:22 +0300

    feat: подключить валидацию celebrate к роутам

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 
+const {
+  validateRegistration,
+  validateAuthorization,
+} = require('../middlewares/validators');
 const { register, login } = require('../controllers/users');
 const userRouter = require('./users');
 const movieRouter = require('./movies');
 const auth = require('../middlewares/auth');
 
-router.post('/signup', register);
-router.post('/signin', login);
+router.post('/signup', validateRegistration, register);
+router.post('/signin', validateAuthorization, login);
 router.use(auth);
 router.use(userRouter);
 router.use(movieRouter);
--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
 
 const {
+  validateCreateMovie,
+  validateObjectId,
+} = require('../middlewares/validators');
+const {
   getMovies,
   createMovie,
   deleteMovieById,
 } = require('../controllers/movies');
 
 router.get('/movies', getMovies);
-router.post('/movies', createMovie);
-router.delete('/movies/:id', deleteMovieById);
+router.post('/movies', validateCreateMovie, createMovie);
+router.delete('/movies/:id', validateObjectId, deleteMovieById);
 
 module.exports = router;
