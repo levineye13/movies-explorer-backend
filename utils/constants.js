d1233f5d36e853c40a29ac47f6b666bf64961dc5
Oleg Zharov <levineye@gmail.com>
2021-02-15 18:19:52 +0300

    feat: добавить заголовок

--- utils/constants.js
+++ utils/constants.js
@@ -11,8 +11,12 @@ module.exports.CORS_OPTIONS = {
     'http://www.movies-explorer.students.nomoreparties.xyz',
     'https://movies-explorer.students.nomoreparties.xyz',
     'https://www.movies-explorer.students.nomoreparties.xyz',
+    'http://api.movies-explorer.students.nomoreparties.xyz',
+    'http://www.api.movies-explorer.students.nomoreparties.xyz',
+    'https://api.movies-explorer.students.nomoreparties.xyz',
+    'https://www.api.movies-explorer.students.nomoreparties.xyz',
   ],
-  allowedHeaders: ['Origin', 'Content-Type', 'Accept'],
+  allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'X-Requested-With'],
   methods: ['OPTIONS', 'HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true,
 };
