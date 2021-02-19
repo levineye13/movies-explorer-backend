87bd52fb5101e976fa82f696db4a6dcb5a97e582
Oleg Zharov <levineye@gmail.com>
2021-02-19 12:42:53 +0300

    feat: добавить контроллер обработки обращения к несуществующему маршруту

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,18 @@ const { register, login } = require('../controllers/users');
 const userRouter = require('./users');
 const movieRouter = require('./movies');
 const auth = require('../middlewares/auth');
+const { NotFoundError } = require('../errors');
+const {
+  HTTP_MESSAGES: { notFound },
+} = require('../utils/constants');
 
 router.post('/signup', validateRegistration, register);
 router.post('/signin', validateAuthorization, login);
 router.use(auth);
 router.use(userRouter);
 router.use(movieRouter);
+router.all('*', () => {
+  throw new NotFoundError(notFound);
+});
 
 module.exports = router;
